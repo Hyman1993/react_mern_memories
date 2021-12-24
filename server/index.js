6f7de95d711424e754f44afd128a041602bc7eed
Hyman1993 <1392841128@qq.com>
2021-12-24 14:55:45 +0900

    2021.12.24 Environmental Variables

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ import bodyParser from "body-parser";
 import pkg from 'mongoose';
 import cors from "cors";
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv';
 
 const mongoose  = pkg;
 const app = express();
+
+// load configuration from .env file
+dotenv.config();
 
 // must to be set before API be called
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -17,10 +21,10 @@ app.use(cors());
 app.use('/posts',postRoutes);
 
 // https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = "mongodb://root:a5701582@localhost:27017/mern?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+// const CONNECTION_URL = "mongodb://root:a5701582@localhost:27017/mern?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
    .then(() => app.listen(PORT, () => console.log('Server running on port: ' + PORT)))
    .catch((error) => console.log(error.message));
 
